7d227988d87ba54b1bac0db98ce52a030ef8af72
Dzonyy <paszcza.jakub@gmail.com>
2021-01-07 19:29:54 +0100

    added user create or update form

---
+++ app/forms/application_form.rb
@@ -0,0 +1,4 @@
+require 'dry-struct'
+
+class ApplicationForm < Dry::Struct
+end
---
+++ app/forms/users/create_or_update_form.rb
@@ -0,0 +1,11 @@
+module Users
+  class CreateOrUpdateForm < ApplicationForm
+    transform_keys(&:to_sym)
+
+    attribute :email, Types::Coercible::String
+    attribute :password_digest, Types::Coercible::String
+    attribute :is_admin, Types::Nominal::Bool
+    attribute :first_name, Types::Coercible::String
+    attribute :last_name, Types::Coercible::String
+  end
+end
