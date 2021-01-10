b34df0a3da776f64ce4440367cdf25eea383e5c9
Dzonyy <paszcza.jakub@gmail.com>
2021-01-10 12:24:16 +0100

    setting up annotate, anottate all models

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: trips
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  end_at      :datetime
+#  start_at    :datetime
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_trips_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Trip < ApplicationRecord
   belongs_to :user
 end
