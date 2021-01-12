2d3f733e1aa9f26dbe828e8dbeede40256ad797e
Dzonyy <paszcza.jakub@gmail.com>
2021-01-12 17:32:27 +0100

    added create facade

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -2,6 +2,10 @@ module Api
   module V1
     class TripsController < ApplicationController
       include Dry::Monads[:result]
+
+      def new
+        @facade = Trips::CreateFacade.new
+      end
     end
   end
 end
---
+++ app/facades/trips/create_facade.rb
@@ -0,0 +1,33 @@
+module Trips
+  class CreateFacade
+    extend Dry::Initializer
+
+    option :params, optional: true, default: -> { {} }
+
+    def operation
+      @operation ||= Trips::CreateOperation.new(
+        user: user,
+        form: form,
+        contract: contract
+      )
+    end
+
+    def form
+      @form ||= Trips::CreateForm.new(form_data)
+    end
+
+    def contract
+      @contract ||= Trips::CreateContract.new
+    end
+
+    def trip
+      @trip ||= Trip.new
+    end
+
+    private
+
+    def form_data
+      @form_data = HashData::Trips::CreateFactory.call(params: params)
+    end
+  end
+end
