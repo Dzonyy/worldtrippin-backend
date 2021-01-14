ea7ae3cbb723cd22f7301693de88391f21c55b8d
Dzonyy <paszcza.jakub@gmail.com>
2021-01-14 22:02:30 +0100

    fixed user validation

--- app/contracts/users/create_contract.rb
+++ app/contracts/users/create_contract.rb
@@ -10,6 +10,8 @@ module Users
       unless /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.match?(values[:email])
         key.failure('has invalid format')
       end
+
+      key.failure('Email is already taken') if User.find_by(email: values[:email]).present?
     end
   end
 end
